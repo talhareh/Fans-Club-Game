c27d8fcec56b7f56039a2020520807f6c8b8d2a9
talhareh <talha59@gmail.com>
2024-07-11 17:50:40 +0400

    telegram game web3

---
+++ src/store/userSlice.jsx
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    userId: null, 
+    userName: '',
+    totalTaps:0,
+}
+
+const userSlice = createSlice({
+    name : 'user',
+    initialState, 
+    reducers:{
+        setUser: (state, action) =>{
+            state.userId = action.payload.userId,
+            state.userName = action.payload.userName
+        }
+    }
+})
+
+export const {setUser} = userSlice.actions;
+
+export default userSlice.reducer;
